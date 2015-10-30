d62e79afb85eb0dff29e2e2eaf93d0f65bdf2eb4
Daisuke Fujita <dtanshi45@gmail.com>
2015-10-30 16:45:57 +0900

    Generate Dockerfile of Service

--- lib/msplex/resource/service.rb
+++ lib/msplex/resource/service.rb
@@ -14,6 +14,29 @@ module Msplex
         }
       end
 
+      def dockerfile
+        <<-DOCKERFILE
+FROM #{image}
+MAINTAINER Your Name <you@example.com>
+
+RUN bundle config --global frozen 1
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+ADD Gemfile /usr/src/app/
+ADD Gemfile.lock /usr/src/app/
+RUN bundle install --without test development --system
+
+ADD . /usr/src/app
+
+RUN apt-get update && apt-get install -y nodejs --no-install-recommends && rm -rf /var/lib/apt/lists/*
+
+EXPOSE 9292
+CMD ["bundle", "exec", "rackup", "-p", "9292", "-E", "production"]
+        DOCKERFILE
+      end
+
       def image
         "ruby:2.2.3"
       end
--- spec/lib/msplex/resource/service_spec.rb
+++ spec/lib/msplex/resource/service_spec.rb
@@ -25,6 +25,33 @@ module Msplex
         end
       end
 
+      describe "#dockerfile" do
+        subject { service.dockerfile }
+
+        it "should generate Dockerfile" do
+          expect(subject).to eq(<<-DOCKERFILE)
+FROM ruby:2.2.3
+MAINTAINER Your Name <you@example.com>
+
+RUN bundle config --global frozen 1
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+ADD Gemfile /usr/src/app/
+ADD Gemfile.lock /usr/src/app/
+RUN bundle install --without test development --system
+
+ADD . /usr/src/app
+
+RUN apt-get update && apt-get install -y nodejs --no-install-recommends && rm -rf /var/lib/apt/lists/*
+
+EXPOSE 9292
+CMD ["bundle", "exec", "rackup", "-p", "9292", "-E", "production"]
+          DOCKERFILE
+        end
+      end
+
       describe "#image" do
         subject { service.image }
 
