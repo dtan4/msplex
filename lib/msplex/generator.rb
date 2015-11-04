6c5c5bca97fa7bafda78e83c30e524784fb26266
Daisuke Fujita <dtanshi45@gmail.com>
2015-11-04 22:28:04 +0900

    Generate entire docker-compose.yml

--- lib/msplex/generator.rb
+++ lib/msplex/generator.rb
@@ -1,12 +1,35 @@
 module Msplex
   class Generator
-    attr_reader :application, :frontend, :services, :databases
+    attr_reader :application, :frontend, :services, :databases, :out_dir
 
-    def initialize(application, frontend, services, databases)
+    def initialize(application, frontend, services, databases, out_dir)
       @application = application
       @frontend = frontend
       @services = services
       @databases = databases
+      @out_dir = out_dir
+    end
+
+    def compose
+      compose_yml = { frontend: @frontend.compose(@services) }
+      service_database_pairs.each { |service, database| compose_yml[service.name] = service.compose(database) }
+      @databases.each { |database| compose_yml[database.name] = database.compose }
+      write_compose(compose_yml)
+    end
+
+    private
+
+    def service_database_pairs
+      @service_database_pairs ||= @application.links.map do |link|
+        service, database = *link.split(":")
+        [@services.find { |s| s.name == service }, @databases.find { |d| d.name == database }]
+      end
+    end
+
+    def write_compose(compose_yml)
+      open(File.join(File.expand_path(out_dir), "docker-compose.yml"), "w+") do |f|
+        f.puts Utils.desymbolize_keys(compose_yml).to_yaml.lines[1..-1]
+      end
     end
   end
 end
---
+++ spec/lib/msplex/generator_spec.rb
@@ -0,0 +1,127 @@
+require "spec_helper"
+
+module Msplex
+  describe Generator do
+    shared_context :uses_temp_dir do
+      around do |example|
+        Dir.mktmpdir do |dir|
+          @out_dir = dir
+          example.run
+        end
+      end
+
+      attr_reader :out_dir
+    end
+
+    include_context :uses_temp_dir
+
+    let(:application) do
+      double(:application,
+        name: "sample",
+        maintainer: "Tanaka Taro",
+        links: [
+          "hogeservice:hogedb",
+          "fugaservice:fugadb",
+        ]
+      )
+    end
+
+    let(:frontend) do
+      double(:frontend,
+        compose: {
+          image: "ruby:2.2.3",
+          links: [
+            "hoge:hoge",
+            "fuga:fuga",
+          ]
+        }
+      )
+    end
+
+    let(:services) do
+      [
+        double(:service,
+          name: "hogeservice",
+          compose: {
+            image: "ruby:2.2.3",
+            links: [
+              "hogedb:db",
+            ],
+            environment: [
+              "RACK_ENV=production",
+            ]
+          }
+        ),
+        double(:service,
+          name: "fugaservice",
+          compose: {
+            image: "ruby:2.2.3",
+            links: [
+              "fugadb:db",
+            ],
+            environment: [
+              "RACK_ENV=production",
+            ]
+          }
+        )
+      ]
+    end
+
+    let(:databases) do
+      [
+        double(:database,
+          name: "hogedb",
+          compose: {
+            image: "postgres:9.4",
+          }
+        ),
+        double(:database,
+          name: "fugadb",
+          compose: {
+            image: "postgres:9.4",
+          }
+        )
+      ]
+    end
+
+    let(:generator) do
+      described_class.new(application, frontend, services, databases, out_dir)
+    end
+
+    describe "#compose" do
+      subject { generator.compose }
+
+      it "should generate docker-compose.yml" do
+        subject
+        expect(File.exists?(File.join(out_dir, "docker-compose.yml"))).to be true
+      end
+
+      it "should combine docker-compose.yml of all resources" do
+        subject
+        expect(open(File.join(out_dir, "docker-compose.yml")).read).to eq <<-COMPOSE
+frontend:
+  image: ruby:2.2.3
+  links:
+  - hoge:hoge
+  - fuga:fuga
+hogeservice:
+  image: ruby:2.2.3
+  links:
+  - hogedb:db
+  environment:
+  - RACK_ENV=production
+fugaservice:
+  image: ruby:2.2.3
+  links:
+  - fugadb:db
+  environment:
+  - RACK_ENV=production
+hogedb:
+  image: postgres:9.4
+fugadb:
+  image: postgres:9.4
+COMPOSE
+      end
+    end
+  end
+end
