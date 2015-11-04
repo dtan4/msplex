cdf8f9efbfb199fcb9698d45be923d2f126fb150
Daisuke Fujita <dtanshi45@gmail.com>
2015-11-04 22:27:48 +0900

    Add links field to Application

--- lib/msplex/resource/application.rb
+++ lib/msplex/resource/application.rb
@@ -1,12 +1,12 @@
 module Msplex
   module Resource
     class Application
-      attr_reader :name
-      attr_reader :maintainer
+      attr_reader :name, :maintainer, :links
 
-      def initialize(application_name, maintainer)
+      def initialize(application_name, maintainer, links)
         @application_name = application_name
         @maintainer = maintainer
+        @links = links
       end
     end
   end
