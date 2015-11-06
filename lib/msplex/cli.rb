558054e5493828c067fb3cc317bf4e5992e91a21
Daisuke Fujita <dtanshi45@gmail.com>
2015-11-06 14:12:43 +0900

    Implement `msplex generate` command

--- lib/msplex/cli.rb
+++ lib/msplex/cli.rb
@@ -2,17 +2,24 @@ module Msplex
   class Cli < Thor
     desc "new", "Create a new application"
     def new
-      puts "new"
+      raise NotImplementedError
     end
 
     desc "service", "Create a new service"
     def service
-      puts "service"
+      raise NotImplementedError
     end
 
     desc "generate", "Generate application codes"
+    option :schema_dir, type: :string, desc: "Schema directory", aliases: :d
+    option :out_dir, type: :string, desc: "Directory for output artifacts", aliases: :o
     def generate
-      puts "genrate"
+      application, frontend, services, databases = Msplex.read_schema_directory(options[:schema_dir])
+      generator = Msplex::Generator.new(application, frontend, services, databases, options[:out_dir])
+
+      generator.generate_compose
+      generator.generate_frontend
+      generator.generate_services
     end
   end
 end
