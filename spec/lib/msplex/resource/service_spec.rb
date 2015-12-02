8a1a5f675eaf161841bc02ce9f22256053ee6675
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-02 10:58:13 +0900

    Modify the order of installing nodejs in service

--- spec/lib/msplex/resource/service_spec.rb
+++ spec/lib/msplex/resource/service_spec.rb
@@ -191,6 +191,8 @@ ENV RACK_ENV production
 
 RUN bundle config --global frozen 1
 
+RUN apt-get update && apt-get install -y nodejs --no-install-recommends && rm -rf /var/lib/apt/lists/*
+
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
@@ -199,8 +201,6 @@ ADD Gemfile.lock /usr/src/app/
 RUN bundle install --without test development --system
 
 ADD . /usr/src/app
-
-RUN apt-get update && apt-get install -y nodejs --no-install-recommends && rm -rf /var/lib/apt/lists/*
 
 EXPOSE 9292
 CMD ["bundle", "exec", "rackup", "-p", "9292", "-E", "production"]
