bb83a052e77a0b0e368a13c9db5eac16ec0f2403
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-02 11:02:08 +0900

    Stop installing nodejs in service

--- lib/msplex/resource/service.rb
+++ lib/msplex/resource/service.rb
@@ -105,8 +105,6 @@ ENV RACK_ENV production
 
 RUN bundle config --global frozen 1
 
-RUN apt-get update && apt-get install -y nodejs --no-install-recommends && rm -rf /var/lib/apt/lists/*
-
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
