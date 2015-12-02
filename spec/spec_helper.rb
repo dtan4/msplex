1b8b9b80503b3b6769066d41ded8755490591fc3
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-02 09:41:58 +0900

    Make it compatible with simplecov 0.11.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 require 'codeclimate-test-reporter'
 
 SimpleCov.start do
-  formatter SimpleCov::Formatter::MultiFormatter[
+  formatter SimpleCov::Formatter::MultiFormatter.new([
     SimpleCov::Formatter::HTMLFormatter,
     CodeClimate::TestReporter::Formatter,
-  ]
+  ])
 end
 
 require 'rspec/its'
